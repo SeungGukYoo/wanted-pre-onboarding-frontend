41949229791afca0d5ef849bdacfb8b8f5133789
SeungGukYoo <dbtmd878@gmail.com>
2023-07-29 15:34:32 +0900

    로그인시 Todo페이지로 이동 기능 구현

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await axios({
+      url: "https://www.pre-onboarding-selection-task.shop/auth/signin",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        email,
+        password,
+      },
+    })
+      .then((result) => {
+        if (result.status === 200 && result.data.access_token) {
+          alert("환영합니다.");
+          localStorage.setItem("jwt_token", result.data.access_token);
+          navigate("/todo");
+        }
+      })
+      .catch((err) => alert(err.response.data));
+  };
+  useEffect(() => {
+    if (localStorage.getItem("jwt_token")) navigate("/todo");
+  }, [navigate]);
   return (
     <div className="bg-[#EDF2F7] h-[100vh]  flex justify-center items-center">
-      <form className="min-w-[520px] bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form className="min-w-[520px] bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSignIn}>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="">
             Email
@@ -13,6 +44,8 @@ const SignIn = () => {
             type="text"
             data-testid="email-input"
             placeholder="example@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
         <div className="mb-6">
@@ -24,13 +57,16 @@ const SignIn = () => {
             type="password"
             data-testid="password-input"
             placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <p className="text-red-500 text-xs italic">Please choose a password.</p>
+          {error && <p className="text-red-500 text-xs italic">Please choose a password.</p>}
         </div>
         <div className="flex items-center justify-end">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             data-testid="signin-button"
+            type="submit"
           >
             Sign In
           </button>
