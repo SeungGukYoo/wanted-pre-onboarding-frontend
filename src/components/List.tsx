6655ef921898f6b42779fb8132ee8cc68dbb0a94
SeungGukYoo <dbtmd878@gmail.com>
2023-07-29 14:57:16 +0900

    메인화면, 로그인 UI 생성

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,23 +2,37 @@ import React from "react";
 
 const List = () => {
   return (
-    <div>
-      <div>
-        <input type="text" className="border border-black" />
-        <button>ADD</button>
+    <div className="max-w-[1024px] mx-auto flex flex-col justify-center items-center">
+      <div className="my-10 flex items-center border-b border-teal-500 py-2">
+        <input
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="Jane Doe"
+          aria-label="Full name"
+        />
+        <button
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          type="button"
+        >
+          ADD
+        </button>
       </div>
       <ul className="">
-        <li className="flex">
-          <input type="checkbox" name="" id="" />
-          <p>todo1</p>
-          <button>수정</button>
-          <button>삭제</button>
+        <li className="flex my-4 py-2 border-y-2 border-gray-700/30 ">
+          <input className="mr-4" type="checkbox" name="" id="" />
+          <p className=" text-[24px] mr-4 min-w-[250px] line-clamp-1">todo1fadsfasdfsfdsfdasfasdf</p>
+          <button className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+            수정
+          </button>
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">삭제</button>
         </li>
-        <li>
-          <input type="checkbox" name="" id="" />
-          <p>todo1</p>
-          <button>수정</button>
-          <button>삭제</button>
+        <li className="flex my-4 py-2 border-y-2 border-gray-700/30 ">
+          <input className="mr-4" type="checkbox" name="" id="" />
+          <p className=" text-[24px] mr-4 min-w-[250px] line-clamp-1">todo1fadsfasdfsfdsfdasfasdf</p>
+          <button className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+            수정
+          </button>
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">삭제</button>
         </li>
       </ul>
     </div>
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const SignIn = () => {
-  return <div>signin page</div>;
+  return (
+    <div className="bg-[#EDF2F7] h-[100vh]  flex justify-center items-center">
+      <form className="min-w-[520px] bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            data-testid="email-input"
+            placeholder="example@email.com"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            data-testid="password-input"
+            placeholder="password"
+          />
+          <p className="text-red-500 text-xs italic">Please choose a password.</p>
+        </div>
+        <div className="flex items-center justify-end">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            data-testid="signin-button"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
