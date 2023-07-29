b8e950462efc8548f613729f911a98f7ed435308
SeungGukYoo <dbtmd878@gmail.com>
2023-07-29 16:50:03 +0900

    todo 목록 보여주기 위한 기능 추가, 각각의 todo 컴포넌트로 분리

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,39 +1,85 @@
-import React from "react";
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Todo from "./Todo";
+
+interface TodoType {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
 
 const List = () => {
+  const navigation = useNavigate();
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState<TodoType[] | []>(() => {
+    const localStorageTodos = localStorage.getItem("todos");
+    return localStorageTodos ? JSON.parse(localStorageTodos) : [];
+  });
+  const signOut = useCallback(() => {
+    localStorage.removeItem("jwt_token");
+    localStorage.removeItem("todos");
+    navigation("/");
+  }, [navigation]);
+
+  const addTodo = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    await axios({
+      url: "https://www.pre-onboarding-selection-task.shop/todos",
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("jwt_token")}`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        todo,
+      },
+    })
+      .then((result) => {
+        setTodos(() => [...todos, result.data]);
+        setTodo("");
+      })
+      .catch((err) => console.log(err.response));
+  };
+  useEffect(() => {
+    if (!localStorage.getItem("jwt_token")) navigation("/");
+  }, [navigation]);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  console.log(todos);
   return (
     <div className="max-w-[1024px] mx-auto flex flex-col justify-center items-center">
+      <button
+        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 mt-8 rounded"
+        onClick={signOut}
+      >
+        Sign Out
+      </button>
       <div className="my-10 flex items-center border-b border-teal-500 py-2">
         <input
           className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
-          placeholder="Jane Doe"
-          aria-label="Full name"
+          placeholder="example"
+          data-testid="new-todo-input"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
         />
         <button
           className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
           type="button"
+          data-testid="new-todo-add-button"
+          onClick={addTodo}
         >
           ADD
         </button>
       </div>
       <ul className="">
-        <li className="flex my-4 py-2 border-y-2 border-gray-700/30 ">
-          <input className="mr-4" type="checkbox" name="" id="" />
-          <p className=" text-[24px] mr-4 min-w-[250px] line-clamp-1">todo1fadsfasdfsfdsfdasfasdf</p>
-          <button className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
-            수정
-          </button>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">삭제</button>
-        </li>
-        <li className="flex my-4 py-2 border-y-2 border-gray-700/30 ">
-          <input className="mr-4" type="checkbox" name="" id="" />
-          <p className=" text-[24px] mr-4 min-w-[250px] line-clamp-1">todo1fadsfasdfsfdsfdasfasdf</p>
-          <button className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
-            수정
-          </button>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">삭제</button>
-        </li>
+        {todos.map((todo) => (
+          <Todo key={todo.id} data={todo} />
+        ))}
       </ul>
     </div>
   );
