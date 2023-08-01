e95c1855425a823102be6f194f15ac0861066a9c
SeungGukYoo <dbtmd878@gmail.com>
2023-08-01 14:35:55 +0900

    타입 분리, 에러 처리 코드 수정, CSS수정

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,10 +1,8 @@
+import axios from "axios";
 import React, { useCallback, useRef, useState } from "react";
+import type { Prop } from "../..";
 import { GlobalState } from "../context/TodoProvider";
 import { DeleteTodo, UpdateTodo } from "../util/TodoUtil";
-
-interface Prop {
-  data: { id: number; todo: string; isCompleted: boolean; userId: number };
-}
 
 const Todo = ({ data }: Prop) => {
   const context = GlobalState();
@@ -15,12 +13,20 @@ const Todo = ({ data }: Prop) => {
   const deleteTodo = useCallback(
     async (e: React.FormEvent) => {
       e.preventDefault();
-      await DeleteTodo(data.id).then((_) => {
-        context?.dispatch({
-          type: "DELETE",
-          payload: data.id,
+      try {
+        await DeleteTodo(data.id).then((_) => {
+          context?.dispatch({
+            type: "DELETE",
+            payload: data.id,
+          });
         });
-      });
+      } catch (err) {
+        if (axios.isAxiosError(err) && err.response?.data) {
+          alert(err.response.data.message);
+        } else {
+          console.error(err);
+        }
+      }
     },
     [context, data.id]
   );
@@ -28,33 +34,51 @@ const Todo = ({ data }: Prop) => {
     async (e: React.FormEvent) => {
       e.preventDefault();
       if (checkRef.current && textRef.current && isModify) {
-        await UpdateTodo({
+        if (!textRef.current.value.trim()) {
+          alert("공백은 저장이 불가능합니다.");
+          return;
+        }
+      }
+      try {
+        const result = await UpdateTodo({
           id: data.id,
-          todo: textRef.current?.value,
-          isCompleted: checkRef.current?.checked,
-        }).then((result) => {
-          context?.dispatch({
-            type: "UPDATE",
-            payload: result,
-          });
-          setIsModify(false);
+          todo: textRef.current?.value as string,
+          isCompleted: checkRef.current?.checked as boolean,
         });
+        context?.dispatch({
+          type: "UPDATE",
+          payload: result,
+        });
+        setIsModify(false);
+      } catch (err) {
+        if (axios.isAxiosError(err) && err.response?.data) {
+          alert(err.response.data.message);
+        } else {
+          console.log(err);
+        }
       }
     },
     [context, data.id, isModify]
   );
 
   const checkBoxUpdate = useCallback(async () => {
-    if (checkRef.current && !isModify) {
-      await UpdateTodo({
-        ...data,
-        isCompleted: checkRef.current?.checked,
-      }).then((result) => {
+    try {
+      if (checkRef.current && !isModify) {
+        const result = await UpdateTodo({
+          ...data,
+          isCompleted: checkRef.current?.checked,
+        });
         context?.dispatch({
           type: "UPDATE",
           payload: result,
         });
-      });
+      }
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response?.data) {
+        alert(err.response.data.message);
+      } else {
+        console.error(err);
+      }
     }
   }, [context, data, isModify]);
   return (
@@ -68,26 +92,29 @@ const Todo = ({ data }: Prop) => {
       />
       {isModify ? (
         <input
-          className="text-[24px] mr-4 min-w-[250px] line-clamp-1"
+          className="text-[24px] mr-4 min-w-[250px] line-clamp-1 border-teal-500 border-b-2 outline-none font-light"
           type="text"
           defaultValue={data.todo}
           ref={textRef}
+          data-testid="modify-input"
         ></input>
       ) : (
-        <p className="text-[24px] mr-4 min-w-[250px] line-clamp-1">{data.todo}</p>
+        <p className="text-[24px] mr-4 min-w-[250px] line-clamp-1 align-middle leading-loose">{data.todo}</p>
       )}
 
       {isModify ? (
         <button
-          className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          className="mr-2 border-2 border-blue-500 text-blue-600/70 hover:bg-blue-500 hover:text-white font-bold py-2 px-4 rounded"
           onClick={updateTodo}
+          data-testid="submit-button"
         >
-          완료
+          제출
         </button>
       ) : (
         <button
-          className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          className="mr-2 border-2  border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white font-bold py-2 px-4 rounded"
           onClick={() => setIsModify(!isModify)}
+          data-testid="modify-button"
         >
           수정
         </button>
@@ -95,15 +122,17 @@ const Todo = ({ data }: Prop) => {
 
       {isModify ? (
         <button
-          className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          className="mr-2 border-2 border-red-500/70 text-red-500/70 hover:bg-red-500 hover:text-white font-bold py-2 px-4 rounded-l"
           onClick={() => setIsModify(false)}
+          data-testid="cancel-button"
         >
           취소
         </button>
       ) : (
         <button
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+          className="border-2 border-red-500/70 text-red-500/70 hover:bg-red-500 hover:text-white  font-bold py-2 px-4 rounded"
           onClick={deleteTodo}
+          data-testid="delete-button"
         >
           삭제
         </button>
