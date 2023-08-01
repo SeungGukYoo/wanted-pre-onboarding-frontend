55d464550205314412bf12801d034a3f5be51128
SeungGukYoo <dbtmd878@gmail.com>
2023-08-01 14:37:13 +0900

    커스텀훅을 이용한 로그인 처리, 에러 처리 코드 수정

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,37 +1,40 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import Vaildate from "../custom/Vaildate";
+import { SignInHandle } from "../util/UserUtil";
 
 const SignIn = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { email, setEmail, password, setPassword, enableButton, handleEmailChange, handlePasswordChange } = Vaildate();
+
   const [error, setError] = useState("");
-  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault();
-    await axios({
-      url: "https://www.pre-onboarding-selection-task.shop/auth/signin",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        email,
-        password,
-      },
-    })
-      .then((result) => {
-        if (result.status === 200 && result.data.access_token) {
-          alert("환영합니다.");
-          localStorage.setItem("jwt_token", result.data.access_token);
-          navigate("/todo");
-        }
-      })
-      .catch((err) => alert(err.response.data));
+    if (!enableButton) return;
+    try {
+      const result = await SignInHandle(email, password);
+      if (result.status === 200 && result.data.access_token) {
+        alert("환영합니다.");
+        localStorage.setItem("jwt_token", result.data.access_token);
+        setError("");
+        setEmail("");
+        setPassword("");
+        navigate("/todo");
+      }
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response) {
+        setError(err.response.data.message);
+      } else {
+        console.error(err);
+      }
+    }
   };
   useEffect(() => {
     if (localStorage.getItem("jwt_token")) navigate("/todo");
   }, [navigate]);
+
   return (
     <div className="bg-[#EDF2F7] h-[100vh]  flex justify-center items-center">
       <form className="min-w-[520px] bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSignIn}>
@@ -44,7 +47,7 @@ const SignIn = () => {
             type="text"
             data-testid="email-input"
             placeholder="example@email.com"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => handleEmailChange(e)}
             value={email}
           />
         </div>
@@ -58,15 +61,18 @@ const SignIn = () => {
             data-testid="password-input"
             placeholder="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => handlePasswordChange(e)}
           />
-          {error && <p className="text-red-500 text-xs italic">Please choose a password.</p>}
+          {error && <p className="text-red-500 text-xs italic">{error}</p>}
         </div>
         <div className="flex items-center justify-end">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className={`bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+              enableButton ? " hover:bg-blue-700" : "opacity-50 cursor-not-allowed"
+            }`}
             data-testid="signin-button"
             type="submit"
+            disabled={enableButton ? false : true}
           >
             Sign In
           </button>
